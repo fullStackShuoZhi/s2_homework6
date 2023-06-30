30bd4d5ec6385c0d8e3c0136dc7665b5bc9d5ad8
shuozhi <shuozhi.zp@wayxtech.com>
2023-06-30 19:14:07 +0800

    feature: subkey生成后，修改各项配置，重新build spec，并添加各项脚本

--- scripts/insert_keys.sh
+++ scripts/insert_keys.sh
@@ -1,86 +1,6 @@
 #!/bin/sh
-#./target/release/node-template key insert \
-#  --base-path /tmp/node0 \
-#  --chain kictto-chain-raw.json \
-#  --scheme Sr25519 \
-#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
-#  --password-interactive \
-#  --key-type babe
-#
-#./target/release/node-template key insert \
-#  --base-path /tmp/node0 \
-#  --chain kictto-chain-raw.json \
-#  --scheme Ed25519 \
-#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
-#  --password-interactive \
-#  --key-type gran
-#
-#./target/release/node-template key insert \
-#  --base-path /tmp/node1 \
-#  --chain kictto-chain-raw.json \
-#  --scheme Sr25519 \
-#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
-#  --password-interactive \
-#  --key-type babe
-#
-#./target/release/node-template key insert \
-#  --base-path /tmp/node1 \
-#  --chain kictto-chain-raw.json \
-#  --scheme Ed25519 \
-#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
-#  --password-interactive \
-#  --key-type gran
-#
-#./target/release/node-template key insert \
-#  --base-path /tmp/node2 \
-#  --chain kictto-chain-raw.json \
-#  --scheme Sr25519 \
-#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
-#  --password-interactive \
-#  --key-type babe
-#
-#./target/release/node-template key insert \
-#  --base-path /tmp/node2 \
-#  --chain kictto-chain-raw.json \
-#  --scheme Ed25519 \
-#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
-#  --password-interactive \
-#  --key-type gran
-#
-#./target/release/node-template key insert \
-#  --base-path /tmp/node3 \
-#  --chain kictto-chain-raw.json \
-#  --scheme Sr25519 \
-#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
-#  --password-interactive \
-#  --key-type babe
-#
-#./target/release/node-template key insert \
-#  --base-path /tmp/node3 \
-#  --chain kictto-chain-raw.json \
-#  --scheme Ed25519 \
-#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
-#  --password-interactive \
-#  --key-type gran
-#
-#./target/release/node-template key insert \
-#  --base-path /tmp/node4 \
-#  --chain kictto-chain-raw.json \
-#  --scheme Sr25519 \
-#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
-#  --password-interactive \
-#  --key-type babe
-#
-#./target/release/node-template key insert \
-#  --base-path /tmp/node4 \
-#  --chain kictto-chain-raw.json \
-#  --scheme Ed25519 \
-#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
-#  --password-interactive \
-#  --key-type gran
-
 ./target/release/node-template key insert \
-  --base-path /tmp/node5 \
+  --base-path /tmp/node0 \
   --chain kictto-chain-raw.json \
   --scheme Sr25519 \
   --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
@@ -88,9 +8,89 @@
   --key-type babe
 
 ./target/release/node-template key insert \
-  --base-path /tmp/node5 \
+  --base-path /tmp/node0 \
   --chain kictto-chain-raw.json \
   --scheme Ed25519 \
   --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
   --password-interactive \
   --key-type gran
+
+./target/release/node-template key insert \
+  --base-path /tmp/node1 \
+  --chain kictto-chain-raw.json \
+  --scheme Sr25519 \
+  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
+  --password-interactive \
+  --key-type babe
+
+./target/release/node-template key insert \
+  --base-path /tmp/node1 \
+  --chain kictto-chain-raw.json \
+  --scheme Ed25519 \
+  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
+  --password-interactive \
+  --key-type gran
+
+./target/release/node-template key insert \
+  --base-path /tmp/node2 \
+  --chain kictto-chain-raw.json \
+  --scheme Sr25519 \
+  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
+  --password-interactive \
+  --key-type babe
+
+./target/release/node-template key insert \
+  --base-path /tmp/node2 \
+  --chain kictto-chain-raw.json \
+  --scheme Ed25519 \
+  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
+  --password-interactive \
+  --key-type gran
+
+./target/release/node-template key insert \
+  --base-path /tmp/node3 \
+  --chain kictto-chain-raw.json \
+  --scheme Sr25519 \
+  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
+  --password-interactive \
+  --key-type babe
+
+./target/release/node-template key insert \
+  --base-path /tmp/node3 \
+  --chain kictto-chain-raw.json \
+  --scheme Ed25519 \
+  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
+  --password-interactive \
+  --key-type gran
+
+./target/release/node-template key insert \
+  --base-path /tmp/node4 \
+  --chain kictto-chain-raw.json \
+  --scheme Sr25519 \
+  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
+  --password-interactive \
+  --key-type babe
+
+./target/release/node-template key insert \
+  --base-path /tmp/node4 \
+  --chain kictto-chain-raw.json \
+  --scheme Ed25519 \
+  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
+  --password-interactive \
+  --key-type gran
+
+#./target/release/node-template key insert \
+#  --base-path /tmp/node5 \
+#  --chain kictto-chain-raw.json \
+#  --scheme Sr25519 \
+#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
+#  --password-interactive \
+#  --key-type babe
+#
+#./target/release/node-template key insert \
+#  --base-path /tmp/node5 \
+#  --chain kictto-chain-raw.json \
+#  --scheme Ed25519 \
+#  --suri "icon coral pioneer chalk story seek picture tone fetch buzz collect broom" \
+#  --password-interactive \
+#  --key-type gran
